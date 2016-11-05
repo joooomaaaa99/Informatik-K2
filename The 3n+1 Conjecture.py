dafe2d37271086168ce7ddc5ae3bdf08c4b58064
Flowlishes <florianwolf1@Nakama-4.local>
2016-11-05 23:06:50 +0100

    The 3n+1 Conjecture

---
+++ The 3n+1 Conjecture.py
@@ -0,0 +1,27 @@
+#author: Florian Wolf
+#date: 05.11.2016
+
+# coding: utf8
+
+import math
+import sys
+import unicodedata
+
+j = 0 #Zaehler fuer die Rechenschritte
+
+#Obere und untere Grenze eingeben
+for i in range(2,7):
+    print("Die Zahl lautet: " + str(i)+ "\n")
+    while i !=1:
+        if math.fmod(i, 2) == 0:
+            i = i/2
+            print(i)
+            j += 1
+        else:
+            i = (3*i+1) / 2
+            print(i)
+            j += 1
+    else:
+        print("Die Zahl ist geschafft!")
+        print("Benötigte Schritte:"+  str(j) + "\n")
+        j = 0
