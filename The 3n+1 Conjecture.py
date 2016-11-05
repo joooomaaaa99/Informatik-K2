8d46b6e9ac3336eb63685860960df2bf21398b2b
Flowlishes <florianwolf1@Nakama-4.local>
2016-11-05 23:37:08 +0100

    Updated 3n+1 Conjecture

--- The 3n+1 Conjecture.py
+++ The 3n+1 Conjecture.py
@@ -6,10 +6,12 @@
 import math
 
 j = 0 #Zaehler fuer die Rechenschritte
+k = 0 #Zahler um die hoechste Anzahl an Schritten festzustellen
 
 #Obere und untere Grenze eingeben
-for i in range(2,7):
-    print("Die Zahl lautet: " + str(i)+ "\n")
+for i in range(1,20):
+    l = i #Zaehler fuer den Index der Zahl, relevant fuer die untere if-Bedingung
+    print("Die Zahl lautet: " + str(l)+ "\n")
     while i !=1:
         if math.fmod(i, 2) == 0:
             i = i/2
@@ -20,6 +22,13 @@ for i in range(2,7):
             print(i)
             j += 1
     else:
+        n = j
         print("Die Zahl ist geschafft!")
-        print("Benötigte Schritte:"+  str(j) + "\n")
+        print("Benoetigte Schritte: "+  str(j) + "\n")
+        if n > k:
+            k = n
+            q = l #Zaehler fuer den Index der hoechsten Schrittanzahl
+        else:
+            print("")
         j = 0
+print("Die hoechste Anzahl an Schritten lautet " + str(k) + " bei Zahl mit der Nummer " + str(q) + ".")
